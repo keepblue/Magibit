0ce4b0295e25256c90ac099fabba5dd83f4aa318
keepblue-PC\keepblue <1960956485@qq.com>
2018-03-10 17:16:47 +0800

    修改package安装包目录为★Magi

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@
 /**
  * Four Digit Display 模块
  */
-//% weight=100 color=#64C800 icon="日"
-namespace FourDigitDisplay {
+//% weight=100 color=#64C800 icon="★"
+namespace Magibit {
 
     let COMMAND_I2C_ADDRESS = 0x24
     let DISPLAY_I2C_ADDRESS = 0x34
